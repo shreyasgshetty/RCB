0d10d9171b57206dd7ef4495844e149a7626961b
Shreyas G Shetty <shreyasgshetty.18@gmail.com>
2026-03-28 23:22:52 +0530

    Update rcb.js

--- rcb.js
+++ rcb.js
@@ -8,7 +8,11 @@ const CHAT_IDS = [
   '6533691877',   // your phone
   '8137670107',
   '1736583692',
-  '5055670024' // other device / person
+  '5055670024',
+  '6778719534',
+  '6780280722',
+  '6200439674',
+  '1803061111' // other device / person
 ];
 const url = 'https://rcbscaleapi.ticketgenie.in/ticket/eventlist/O';
 
@@ -86,7 +90,7 @@ function fetchData() {
         const currentResponse = JSON.stringify(json);
 
         // ✅ CORRECT change detection
-        if (lastResponse && currentResponse !== lastResponse) {
+        if (lastResponse) {
           clearInterval(intervalId);
           sendTelegramAlert();            // 🔊 START alarm
           return;
